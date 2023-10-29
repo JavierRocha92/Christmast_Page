49cdc975a0fabfb540ef6206995689521f3cac9a
Javier Rocha Gómez <javierrg1992@gmail.com>
2023-10-29 18:48:46 +0100

    page totally functional

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const superior__img = document.getElementById('superior__img')
 const favorites = document.getElementById('favorites')
 const container = document.getElementById('container')
-
+/**
+ * function to add classList getUp, getDown to container depending what classList it has 
+ * and the same to favorites
+ */
 const animationIn = () => {
     if(container.classList.contains('getDown') ||
     container.classList.contains('getUp') ){
@@ -19,16 +22,25 @@ const animationIn = () => {
     }
 }
 
+/**
+ * function to add classList vibration to superior__img
+ */
 const vibrationIn = () => {
     console.log('estou en la funcion')
     superior__img.classList.add('vibration')
 }
-
+/**
+ * function to remove classList vibration to superior__img
+ */
 const vibrationOut = () => {
     superior__img.classList.remove('vibration')
 }
 
-
+//conditional to remove classList to container when reloution of window is more than 1200px
+if(window.outerWidth >= 1200){
+    container.classList.remove('getUp')
+    console.log(container.classList)
+}
 superior__img.addEventListener('click',animationIn)
 superior__img.addEventListener('mouseover',vibrationIn)
 superior__img.addEventListener('mouseout',vibrationOut)
